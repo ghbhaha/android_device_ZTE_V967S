2c0e96046627d652365ad5be128dc614a661ba55
carliv <liviucar@yahoo.com>
2013-08-13 00:23:36 +0300

    update device folder

---
+++ README.md
@@ -0,0 +1,6 @@
+android_device_ZTE_V967S
+========================
+
+ZTE V967S device folder
+
+For creating CWM recovery.
--- device_V967S.mk
+++ device_V967S.mk
@@ -17,6 +17,20 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/advanced_meta_init.project.rc:recovery/root/advanced_meta_init.project.rc \
+    $(LOCAL_PATH)/recovery/advanced_meta_init.rc:recovery/root/advanced_meta_init.rc \
+    $(LOCAL_PATH)/recovery/fstab:recovery/root/fstab \
+    $(LOCAL_PATH)/recovery/init.usb.rc:recovery/root/init.usb.rc \
+    $(LOCAL_PATH)/recovery/meta_init.rc:recovery/root/meta_init.rc \
+    $(LOCAL_PATH)/recovery/sbup.ko:recovery/root/sbup.ko \
+    $(LOCAL_PATH)/recovery/ueventd.rc:recovery/root/ueventd.rc
+
+PRODUCT_PACKAGES += \
+    static_busybox \
+    make_ext4fs \
+    setup_fs
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
